e8eddc9119bdbbba044a515e194b1f8e3ab9e335
Will Ramires <97480446+willramires@users.noreply.github.com>
2024-10-28 08:56:39 -0300

    Add files via upload

---
+++ garantia/index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const { pool } = require('./db/connection')
+const { orders } = require('./models/orders')
+const app = express()
+const port = 3000
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.get("/clientes", (req, res) => {
+    pool.query("SELECT * FROM Clientes;", (error, result) => {
+        if (error) {
+            res.status(400).send("Error");
+            return;
+        }
+        res.json(result.rows);
+    });
+})
+
+app.get("/equipamentos", (req, res) => {
+    pool.query("SELECT e.*, c.nome AS cliente_nome FROM Equipamentos e JOIN Clientes c ON e.cliente_id = c.id;", (error, result) => {
+        if (error) {
+            res.status(400).send("Error");
+            return;
+        }
+        res.json(result.rows);
+    });
+})
+
+app.listen(port, () => {
+    pool.connect().then((client) => {
+        console.log('Connected to the database');
+        client.release();
+    })
+})
